4ec4a456ee41f4100b5d9a77630f89862a203436
Jacob Marthaller <https://github.com/jmarthaller>
2020-12-14 20:27:01 -0500

    basic validations?

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Song.destroy_all
+
+
+Song.create(title: "Hotline Bling", released: true, release_year: 2015, artist_name: "Drake", genre: "Rap")
+Song.create(title: "Hello", released: true, release_year: 2014, artist_name: "Adele", genre: "Pop")
+Song.create(title: "Polka Jamz", released: true, release_year: 1960, artist_name: "Phish", genre: "Jam band")
+
+
+
+
+puts "Data Seeded"
